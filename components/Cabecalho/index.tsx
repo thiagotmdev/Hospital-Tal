c3e4d7a5002e5a829d06d39722d3473bebda3848
Thiago Mendes <thiagomendes.dev@gmail.com>
2023-10-08 18:54:11 -0300

    linkado o logo e nome do hospital

--- components/Cabecalho/index.tsx
+++ components/Cabecalho/index.tsx
@@ -1,12 +1,15 @@
 import style from './Cabecalho.module.css';
 import { Navbar } from '../Navbar';
+import Image from 'next/image';
+import logo from '../../src/images/hopistal_logo.png';
+import Link from 'next/link';
 
-export const Cabecalho = () =>{
-    return(
+export const Cabecalho = () => {
+    return (
         <>
             <div className={style.container}>
-                <div className={style.logo}>...</div>
-                <Navbar/>
+                <Link href='/' className={style.link_logo}><Image src={logo} alt='hospital logo' className={style.logo} /><span className={style.name_h}>Hospital Tal</span></Link>
+                <Navbar />
             </div>
         </>
     )
