a3289f9d4caebf66efd3af49c818271a2298e023
Thiago Mendes <thiagomendes.dev@gmail.com>
2023-10-10 09:08:16 -0300

    Criada section de planos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -60,6 +60,48 @@ export default function Home() {
               <Image src={tele_doctor} alt='Imagem de uma doutora com os braços cruzados'/>
             </div>
           </div>
+
+          <div className={style.global_plans}>
+            <div className={style.global_plans_1}>
+              <h2>Planos Globais</h2>
+              <p>O melhor sistema de saúde  do mundo, agora acessível com o plano de associação global MediPocket</p>
+            </div>
+            <div className={style.global_plans_2}>
+              <div className={style.global_plans_2_card}>
+                <h3 className={style.global_plans_2_card_title}>Global Lite</h3>
+                <p className={style.global_plans_2_card_apartir}>A partir de</p>
+                <p className={style.global_plans_2_card_valor}> R$ 150,00</p>
+                <ul>
+                  <li>Cobertura Global de Saúde EM 50L-3Cr</li>
+                  <li>Conselheiro de Saúde</li>
+                  <li>Medicamentos com desconto</li>
+                  <li>Assistência com visto</li>
+                  <li>Triagem Genética Preventiva</li>
+                  <li>Serviço de concierge global</li>
+                </ul>
+                <div className={style.global_plans_2_card_button}>
+                  Contrate
+                </div>
+              </div>
+              <div className={style.global_plans_2_card}>
+                <h3 className={style.global_plans_2_card_title}>Global Family</h3>
+                <p className={style.global_plans_2_card_apartir}>A partir de</p>
+                <p className={style.global_plans_2_card_valor}> R$ 350,00</p>
+                <ul>
+                  <li>Cobertura Global de Saúde EM 50L-3Cr</li>
+                  <li>Conselheiro de Saúde</li>
+                  <li>Medicamentos com desconto</li>
+                  <li>Assistência com visto</li>
+                  <li>Triagem Genética Preventiva</li>
+                  <li>Serviço de concierge global</li>
+                </ul>
+                <div className={style.global_plans_2_card_button}>
+                  Contrate
+                </div>
+              </div>
+            </div>
+          </div>
+          
       </div>
     </>
   )
